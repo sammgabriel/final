bd64bc29bc6e675446c34c82b2a75d484531565c
Sam Gabriel <sgabriel2@mail.greenriver.edu>
2019-03-09 16:45:14 -0500

    Set up project and Fat-Free

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+//turn on error reporting
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+//require autoload
+require_once('vendor/autoload.php');
+
+//create and instance of the Base class
+$f3 = Base::instance();
+//turn on fat free error reporting
+$f3->set('DEBUG',3);
+
+//define a default route
+$f3->route('GET /', function(){
+
+    //echo "Hello!";
+
+    $template = new Template;
+    echo $template->render('views/home.html');
+});
+
+//run fat free
+$f3->run();
